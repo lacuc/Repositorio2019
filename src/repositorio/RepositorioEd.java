bae967af0cae9140d33fc900cdd8ca718f45f415
21828685 <21828685@N205-21.alumnos.uem.es>
2019-01-16 10:43:45 +0100

    Repositao

---
+++ src/repositorio/RepositorioEd.java
@@ -0,0 +1,12 @@
+package repositorio;
+
+public class RepositorioEd {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Introduce primer repositorio para GitHub: ");
+		System.out.println("Creado por: " );
+
+	}
+
+}
